8df4193f2f014bff55d25eebb84836043cda2cc1
Sebastian Vargas <vargas.sebastianm@gmail.com>
2020-09-25 21:15:40 -0300

    Signed-off-by: Sebastian Vargas <vargas.sebastianm@gmail.com>

---
+++ modifica2.php
@@ -0,0 +1,16 @@
+<title>Problema</title>
+</head>
+<body>
+<?php
+// trabaja con formodif.php y modifica1.php
+$conexion=mysqli_connect("localhost","root","","base1") or
+    die("Problemas con la conexión");
+
+mysqli_query($conexion, "update alumnos
+                          set mail='$_REQUEST[mailnuevo]' 
+                        where mail='$_REQUEST[mailviejo]'") or
+  die("Problemas en el select:".mysqli_error($conexion));
+  echo "El mail fue modificado con exito";
+?>
+</body>
+</html>
